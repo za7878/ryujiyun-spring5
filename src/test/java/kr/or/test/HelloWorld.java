3adbe306509d29f741232e66532d83c676fbb139
user <user@SAMSUNG>
2021-05-26 10:37:56 +0900

    어제자 커밋

--- src/test/java/kr/or/test/HelloWorld.java
+++ src/test/java/kr/or/test/HelloWorld.java
@@ -3,8 +3,8 @@ package kr.or.test;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// main 자바프로그램의 진입 메서드
-		System.out.println("Hello Java!");
+		// main 자바프로그램의 진입 메서드 필요
+	System.out.println("헬로 자바~!");
 
 	}
 
