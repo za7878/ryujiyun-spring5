e07110c9f23c4b9ebf339b7bf8ea62626a06fdc7
user <user@SAMSUNG>
2021-06-01 17:41:48 +0900

    20210601화 8교시커밋

--- src/main/java/com/adu/controller/HomeController.java
+++ src/main/java/com/adu/controller/HomeController.java
@@ -1,39 +1,40 @@
 package com.adu.controller;
-
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+//외부 라이브러리(모듈) 사용 = import
+//import org.slf4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
- * Handles requests for the application home page.
+ * 이 클래스는 MVC웹프로젝트를 최초로 생성시, 자동으로 생성되는 클래스.
+ * 웹브라우저의 요청사항을 view단(jsp)에 연결시켜주는 클래스@Controller.
+ * 스프링에서 관리하는 클래스를 스프링빈(콩) = 스프링빈을 명시 @Controller 애노테이션
+ * Beans(콩들) 그래프로 이 프로젝트의 규모를 확인가능.
+ * 스프링이 관리하는 클래스=스프링빈은 파일아이콘에 S가 붙는다.
  */
+
 @Controller
 public class HomeController {
-	
+	//스프링빈(클래스)에서는 로거로 디버그를 한다 = 로거객체를 만든다
+	//로그중slf4j(Spring Log For Java)
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
-	 * Simply selects the home view to render by returning its name.
+	 * 사용자요청(웹브라우저)을 받아서=@RequestMapping 인터페이스를 사용해서 메서드 명을 스프링이 구현함.
+	 * ,route(루트root X)
+	 * return 값으로 view(jsp)를 선택해서 작업한 결과를 변수로 담아서 화면에 전송 후 결과를 표시(렌더링) 한다.
+	 * 폼(자료)전송시 post(자료숨김), get(자료노출-URL쿼리스트링 '?'있는 자료 전송)
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	public String home(Model model) { //콜백메서드, 자동실행됨.
+		String jspVar = "@서비스(DB)에서 처리한 결과";
+		model.addAttribute("jspObject", jspVar );
+		logger.info("디버그 스프링 로고사용: "+jspVar); //System.out 대신 logger 객체를 사용.
+		//home.jsp 파일로 자료를 전송(스프링)하는 기능=model인터페이스 객체 (스프링처리)
+		return "home/index";//확장자가 생략 .jsp가 생략되어 있음.
 	}
 	
 }
